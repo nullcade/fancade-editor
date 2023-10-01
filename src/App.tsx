18c1e722e18cbad3e711b4627380bcf72b70d97d
HoseanRC <hoseandawoodi90@outlook.com>
2023-10-01 21:35:47 +0330

    turned the shit out of it to ts

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import MyThree from './components/MyThree';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MyThree />
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals.ts';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as Element ?? <></>);
 root.render(
   <React.StrictMode>
     <App />
