7bc15f33dcfb055f782f2476b870ac9d15295e46
HoseanRC <hoseandawoodi90@outlook.com>
2023-10-07 11:23:20 +0330

    fixed passing every prop to TextField

--- src/components/ControlledTextField/index.tsx
+++ src/components/ControlledTextField/index.tsx
@@ -1,26 +1,26 @@
 import { TextField, TextFieldProps, TextFieldVariants } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 
-function ControlledTextField(props: {
+function ControlledTextField({setValue, valueCheck, ...props}: {
   variant?: TextFieldVariants,
   setValue: (value: TextFieldProps['value']) => void,
   valueCheck: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => TextFieldProps['value'] | null
 } & Omit<TextFieldProps, 'variant'>) {
-  const [value, setValue] = useState<TextFieldProps['value']>(props.value);
+  const [value, setLocalValue] = useState<TextFieldProps['value']>(props.value);
   useEffect(() => {
-    setValue(props.value);
+    setLocalValue(props.value);
   }, [props.value]);
   return (
     <TextField
       {...props}
       value={value}
       onChange={(event) => {
-        const checkedValue = props.valueCheck(event);
+        const checkedValue = valueCheck(event);
         if (checkedValue !== null)
-          setValue(checkedValue);
+          setLocalValue(checkedValue);
       }}
       onBlur={() => {
-        props.setValue(value);
+        setValue(value);
       }}
     />
   )
