fdbb56fc7361b4910cccfed530235c76321de80c
missingnull <120743119+missingzero@users.noreply.github.com>
2023-10-17 22:21:57 +0200

    use idb for simpler indexeddb api

--- src/components/InfoTab/db.ts
+++ src/components/InfoTab/db.ts
@@ -5,59 +5,27 @@ import {
 } from "../../custom_modules/GameFormat";
 import { deflate, inflate } from "pako";
 import { Buffer } from "buffer";
-
-export function promisifyRequest(request: IDBRequest) {
-  return new Promise((res, rej) => {
-    request.onsuccess = (event) => {
-      res(request.result);
-    };
-    request.onerror = (event) => {
-      rej(event.error);
-    };
-  });
-}
-
-export async function loadDB(name) {
-  const request = indexedDB.open(name, 4);
-  request.onupgradeneeded = (event) => {
-    event.target.result.createObjectStore(name);
-  };
-  return await promisifyRequest(request);
-}
-
-export async function storeGet(store, item) {
-  return await promisifyRequest(store.get(item));
-}
+import {openDB} from "idb"
 
 export async function listGames() {
-  const db = await loadDB("games");
-  const store = db.transaction(["games"], "readwrite").objectStore("games");
-  const request = store.getAllKeys();
-  return await promisifyRequest(request);
+  const db = await openDB("games");
+  return await db.getAllKeys("games");
 }
 
 export async function loadGame(key: string) {
-  const db = await loadDB("games");
-  const store = db.transaction(["games"], "readwrite").objectStore("games");
-
-  const data = await storeGet(store, key);
-  if (!data) return;
+  const db = await openDB("games");
+  const data = await db.get("games", key);
   const game = new GameDecoder(Buffer.from(inflate(data))).decGame();
-  db.close();
   return game;
 }
 
 export async function storeGame(game: Game.Data) {
-  const db = await loadDB("games");
-  const store = db.transaction(["games"], "readwrite").objectStore("games");
-
+  const db = await openDB("games");
   const data = deflate(new GameEncoder(game).encGame());
-  store.put(data, game.title);
-  db.close();
+  await db.put("games", data, game.title);
 }
 
 export async function deleteGame(key: string) {
-  const db = await loadDB("games");
-  const store = db.transaction(["games"], "readwrite").objectStore("games");
-  await promisifyRequest(store.delete(key));
+  const db = await openDB("games");
+  await db.delete("games", key)
 }
