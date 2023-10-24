23dbc8855bba141a8c3de3fb8584a33a5d7cb098
HoseanRC <hoseandawoodi90@outlook.com>
2023-10-24 10:31:22 +0330

    fixed the whole page scrolling on android

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,12 @@ function App() {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          flexWrap: 'nowrap',
         }}
       >
         <TabContext value={tab}>
