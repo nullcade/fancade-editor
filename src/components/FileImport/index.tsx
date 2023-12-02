ee4cf457454703aa0ebb91a795f6f57b469c248a
HoseanRC <hoseandawoodi90@outlook.com>
2023-12-02 22:43:06 +0330

    fixing file import section (keeping zip)

--- src/components/FileImport/index.tsx
+++ src/components/FileImport/index.tsx
@@ -4,8 +4,8 @@ import { unzip } from "unzipit";
 import { storeGame } from "components/InfoTab/db";
 import { Buffer } from "buffer";
 import { Button, ButtonGroup } from "@mui/material";
-import { Publish, FolderZip } from "@mui/icons-material";
-import { GameDecoder, Game } from "custom_modules/GameFormat";
+import { Publish, FolderZip, DataObject } from "@mui/icons-material";
+import { GameDecoder, Game, Chunk } from "custom_modules/GameFormat";
 
 function decode(arr: ArrayBuffer) {
   return new GameDecoder(Buffer.from(zlib.inflate(arr))).decGame();
@@ -21,7 +21,7 @@ function FileImport({
   setStoredGames: React.Dispatch<React.SetStateAction<string[]>>;
 }) {
   const binInput = useRef<HTMLInputElement>(null);
-  const zipInput = useRef<HTMLInputElement>(null);
+  const jsonInput = useRef<HTMLInputElement>(null);
 
   return (
     <ButtonGroup>
@@ -36,39 +36,63 @@ function FileImport({
           type="file"
           id="bin-input"
           ref={binInput}
-          onChange={async () =>
-            binInput.current?.files &&
-            setFile(decode(await binInput.current?.files[0].arrayBuffer()))
-          }
-          style={{ display: "none" }}
-          accept={".fc,.bin"}
-        />
-      </Button>
-      <Button size="small" onClick={() => zipInput.current?.click()}>
-        <FolderZip />
-        <input
-          type="file"
-          id="zip-input"
-          ref={zipInput}
           onChange={async () => {
-            const zip = zipInput.current?.files && await unzip(zipInput.current?.files[0]);
-            if (!zip) return;
-            const entries = Object.values(zip.entries).filter(
-              (e) => !e.name.includes("."),
-            );
-            for await (const entry of entries) {
-              const game = new GameDecoder(
-                Buffer.from(zlib.inflate(await entry.arrayBuffer())),
-              ).decGame();
-              await storeGame(game);
-              if (!storedGames.includes(game.title)) {
-                storedGames.push(game.title);
-                setStoredGames(storedGames);
+            if (!binInput.current?.files) return;
+            if (binInput.current.files[0].name.endsWith(".zip")) {
+              const zip = await unzip(binInput.current.files[0]);
+              const entries = Object.values(zip.entries).filter(
+                (e) => !e.name.includes(".")
+              );
+              for await (const entry of entries) {
+                const game = decode(await entry.arrayBuffer());
+                await storeGame(game);
+                if (!storedGames.includes(game.title)) {
+                  storedGames.push(game.title);
+                  setStoredGames(storedGames);
+                }
               }
+            } else {
+              setFile(decode(await binInput.current?.files[0].arrayBuffer()));
             }
           }}
           style={{ display: "none" }}
-          accept={".zip"}
+          accept={".fc,.bin,.zip"}
+        />
+      </Button>
+      <Button size="small" onClick={() => jsonInput.current?.click()}>
+        <DataObject />
+        <input
+          type="file"
+          id="json-input"
+          ref={jsonInput}
+          onChange={async () => {
+            if (jsonInput.current?.files) {
+              const game: Game.Data = JSON.parse(
+                await jsonInput.current?.files[0].text()
+              );
+              const chunksMap = new Map<number, Chunk.Data>();
+              game.chunks.forEach((chunk, index) => {
+                // chunk.uuid = nanoid();
+                if (chunk.parent) chunk.children = [];
+                if (chunk.name) chunksMap.set(index + game.idOffset, chunk);
+              });
+              game.chunks.forEach((chunk) => {
+                if (chunk.name || !chunk.parent) return;
+                chunksMap.get(chunk.parent)?.children?.push({
+                  uuid: chunk.uuid,
+                  offset: chunk.offset,
+                  faces: chunk.faces,
+                  blocks: chunk.blocks,
+                  values: chunk.values,
+                  wires: chunk.wires,
+                });
+              });
+              game.chunks = [...chunksMap.values()];
+              setFile(game);
+            }
+          }}
+          style={{ display: "none" }}
+          accept={".json"}
         />
       </Button>
     </ButtonGroup>
