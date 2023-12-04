21f207b33e714c6876f061272a79904b8b2e24dd
HoseanRC <hoseandawoodi90@outlook.com>
2023-12-04 07:27:48 +0330

    seems like the error is gone

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,14 +1,19 @@
-window.self.addEventListener('fetch', (event) => {
-    const url = new URL(event.request.url);
-    if (event.request.method === 'POST' && url.pathname === '/') {
-        
-        event.waitUntil(async function () {
-            const client = await window.self.clients.get(event.resultingClientId);
-            const data = await event.request.formData();
-            const files = data.get('file');
-            client.postMessage({ files });
-        }());
-        event.respondWith(Response.redirect('/', 303));
-        return;
-    }
+import { precacheAndRoute } from "workbox-precaching";
+
+precacheAndRoute(window.self.__WB_MANIFEST);
+
+window.self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (event.request.method === "POST" && url.pathname === "/") {
+    event.waitUntil(
+      (async function () {
+        const client = await window.self.clients.get(event.resultingClientId);
+        const data = await event.request.formData();
+        const files = data.get("file");
+        client.postMessage({ files });
+      })()
+    );
+    event.respondWith(Response.redirect("/", 303));
+    return;
+  }
 });
