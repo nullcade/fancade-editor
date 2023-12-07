b961ef784b68dd0bf13152920e33822776d93c8c
HoseanRC <hoseandawoodi90@outlook.com>
2023-12-07 18:10:12 +0330

    bug fixing

--- src/custom_modules/GameFormat/GameDecoder.ts
+++ src/custom_modules/GameFormat/GameDecoder.ts
@@ -32,13 +32,13 @@ export class GameDecoder {
             if (typeof block === "number" && block >= this.idOffset)
               chunk.blocks[xIndex][yIndex][zIndex] =
                 this.uuidMap.get(block) ?? 0;
-          }),
-        ),
+          })
+        )
       );
       if (chunk.name || !chunk.parent) return;
       this.chunksMap.get(chunk.parent)?.children?.push({
         uuid: chunk.uuid,
-        offset: chunk.offset,
+        offset: chunk.offset ?? [0, 0, 0],
         faces: chunk.faces,
         blocks: chunk.blocks,
         values: chunk.values,
@@ -168,16 +168,16 @@ export class GameDecoder {
     const buff = this.read(size);
     return Array.from(
       { length: size * 8 },
-      (_v, i) => (buff[Math.floor(i / 8)] >> i % 8) & 1,
+      (_v, i) => (buff[Math.floor(i / 8)] >> i % 8) & 1
     );
   }
   readGrid<D extends readonly number[], T>(
     dims: readonly [...D],
-    f: (this: this) => T,
+    f: (this: this) => T
   ): Grid<D, T> {
     if (dims.length > 0)
       return Array.from({ length: dims[0] }, () =>
-        this.readGrid(dims.slice(1), f),
+        this.readGrid(dims.slice(1), f)
       ) as any;
     else return f.apply(this) as any;
   }
